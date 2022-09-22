aa718e5c2776a93e0b012cd18c7696e73ca63861
ORCA <ORCx41@gmail.com>
2022-09-22 03:29:44 -0700

    Update main.c

--- ManualResourceDataFetching/main.c
+++ ManualResourceDataFetching/main.c
@@ -7,19 +7,19 @@
 
 BOOL GetResourceData(HMODULE hModule, WORD ResourceId, PVOID* ppResourceRawData, PDWORD psResourceDataSize) {
 
-	CHAR*							pBaseAddr		= (CHAR*)hModule;
-	PIMAGE_DOS_HEADER				pImgDosHdr		= (PIMAGE_DOS_HEADER)pBaseAddr;
-	PIMAGE_NT_HEADERS				pImgNTHdr		= (PIMAGE_NT_HEADERS)(pBaseAddr + pImgDosHdr->e_lfanew);
-	PIMAGE_OPTIONAL_HEADER			pImgOptionalHdr = (PIMAGE_OPTIONAL_HEADER)&pImgNTHdr->OptionalHeader;
-	PIMAGE_DATA_DIRECTORY			pDataDir		= (PIMAGE_DATA_DIRECTORY)&pImgOptionalHdr->DataDirectory[IMAGE_DIRECTORY_ENTRY_RESOURCE];
+	CHAR*			pBaseAddr		= (CHAR*)hModule;
+	PIMAGE_DOS_HEADER 	pImgDosHdr		= (PIMAGE_DOS_HEADER)pBaseAddr;
+	PIMAGE_NT_HEADERS 	pImgNTHdr		= (PIMAGE_NT_HEADERS)(pBaseAddr + pImgDosHdr->e_lfanew);
+	PIMAGE_OPTIONAL_HEADER 	pImgOptionalHdr 	= (PIMAGE_OPTIONAL_HEADER)&pImgNTHdr->OptionalHeader;
+	PIMAGE_DATA_DIRECTORY 	pDataDir		= (PIMAGE_DATA_DIRECTORY)&pImgOptionalHdr->DataDirectory[IMAGE_DIRECTORY_ENTRY_RESOURCE];
 	
-	PIMAGE_RESOURCE_DIRECTORY		pResourceDir	= NULL, pResourceDir2	= NULL, pResourceDir3	= NULL;
+	PIMAGE_RESOURCE_DIRECTORY 	pResourceDir	= NULL, pResourceDir2	= NULL, pResourceDir3	= NULL;
 	PIMAGE_RESOURCE_DIRECTORY_ENTRY pResourceEntry	= NULL, pResourceEntry2 = NULL, pResourceEntry3 = NULL;
 	
-	PIMAGE_RESOURCE_DATA_ENTRY		pResource		= NULL;
+	PIMAGE_RESOURCE_DATA_ENTRY 	pResource		= NULL;
 
 
-	pResourceDir = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress);
+	pResourceDir   = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress);
 	pResourceEntry = (IMAGE_RESOURCE_DIRECTORY_ENTRY*)(pResourceDir + 1);
 
 
@@ -28,17 +28,17 @@ BOOL GetResourceData(HMODULE hModule, WORD ResourceId, PVOID* ppResourceRawData,
 		if (pResourceEntry[i].DataIsDirectory == 0)
 			break;
 		
-		pResourceDir2 = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress + (pResourceEntry[i].OffsetToDirectory & 0x7FFFFFFF));
+		pResourceDir2   = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress + (pResourceEntry[i].OffsetToDirectory & 0x7FFFFFFF));
 		pResourceEntry2 = (PIMAGE_RESOURCE_DIRECTORY_ENTRY)(pResourceDir2 + 1);
 
 		if (pResourceEntry2->DataIsDirectory == 1 && pResourceEntry2->Id == ResourceId){
 
-			pResourceDir3 = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress + (pResourceEntry2->OffsetToDirectory & 0x7FFFFFFF));
+			pResourceDir3   = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress + (pResourceEntry2->OffsetToDirectory & 0x7FFFFFFF));
 			pResourceEntry3 = (PIMAGE_RESOURCE_DIRECTORY_ENTRY)(pResourceDir3 + 1);
 
 			pResource = (PIMAGE_RESOURCE_DATA_ENTRY)(pBaseAddr + pDataDir->VirtualAddress + (pResourceEntry3->OffsetToData & 0x7FFFFFFF));
 
-			*ppResourceRawData	= (PVOID)(pBaseAddr + (pResource->OffsetToData));
+			*ppResourceRawData  = (PVOID)(pBaseAddr + (pResource->OffsetToData));
 			*psResourceDataSize = pResource->Size;
 
 			if (*ppResourceRawData != NULL && *psResourceDataSize != NULL)
